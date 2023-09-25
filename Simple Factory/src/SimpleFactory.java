48deaec45f4af648cd67a8f3182616f5bd74c95e
jaisat <jaiswalsatyam211@gmail.com>
2023-09-25 14:13:56 +0530

    first commit

---
+++ Simple Factory/src/SimpleFactory.java
@@ -0,0 +1,6 @@
+public class SimpleFactory {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Simple Factory Design Pattern");
+    }
+}
