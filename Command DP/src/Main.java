88ff2e9de465f334065f6df25b779519664f4de5
jaisat <jaiswalsatyam211@gmail.com>
2023-10-06 23:34:29 +0530

    Command DP Commands

---
+++ Command DP/src/Main.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.List;
+
+interface ActionListenerCommand{
+    public void execute();
+}
+
+class ActionSaveCommand implements ActionListenerCommand{
+    private Document doc;
+
+    public ActionSaveCommand(Document doc){
+        this.doc = doc;
+    }
+    @Override
+    public void execute() {
+        doc.save();
+    }
+}
+
+class ActionOpenCommand implements ActionListenerCommand{
+    private Document doc;
+
+    public  ActionOpenCommand(Document doc){
+        this.doc = doc;
+    }
+    @Override
+    public void execute() {
+        doc.open();
+    }
+}
+
+class Document{
+    public void open(){
+        System.out.println("Opening the doc");
+    }
+
+    public void save(){
+        System.out.println("Saving the doc");
+    }
+}
+
+class MenuOptions{
+    List<ActionListenerCommand> commands = new ArrayList<>();
+
+    public void addCommand(ActionListenerCommand command){
+        commands.add(command);
+    }
+
+    public void executeCommands(){
+        for(ActionListenerCommand command : commands){
+            command.execute();
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Document doc = new Document();
+        ActionListenerCommand saveCommand = new ActionSaveCommand(doc);
+        ActionListenerCommand openCommand = new ActionOpenCommand(doc);
+        MenuOptions menu = new MenuOptions();
+        menu.addCommand(saveCommand);
+        menu.addCommand(openCommand);
+
+        menu.executeCommands();
+    }
+}
--- Observer Design Pattern/src/Main.java
+++ Observer Design Pattern/src/Main.java
@@ -23,6 +23,5 @@ public class Main {
 
         myOrder.setOrderStatus("Delivered");
 
-
     }
 }
